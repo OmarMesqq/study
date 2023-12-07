6f95f33fdf7e4ccc1d537e4d18cfb7700b30409e
OmarMesqq <omarmsqt@gmail.com>
2023-12-06 23:37:21 -0300

    chore: :sparkles: add museum tree

---
+++ museum/haskell.hs
@@ -0,0 +1,7 @@
+-- Create entrypoint and use the IO monad (type signature)
+-- Do not return anything
+main :: IO ()
+-- Defines the body of the main function
+main = do 
+ putStrLn "Hello, World!" 
+
